48519739714e9a2778e453c16a482c00ac0fe5da
ZikFreeDKarteR44 <john.smithkarter2016@mail.ru>
2023-08-22 17:17:51 +0300

    Add ability to set time for todo

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -2,8 +2,10 @@ class TaskModel {
   TaskModel({
     required this.text,
     required this.id,
+    this.reminderTime,
   });
 
   final String text;
   final String id;
+  final String? reminderTime;
 }
--- lib/widgets/task_item_widget.dart
+++ lib/widgets/task_item_widget.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_fojin/consts/colors.dart';
+import 'package:to_do_fojin/consts/strings.dart';
 import 'package:to_do_fojin/consts/styles.dart';
 
 class TaskItemWidget extends StatelessWidget {
   const TaskItemWidget({
     super.key,
     required this.text,
+    required this.reminderTime,
     required this.onTap,
   });
 
   final String text;
+  final String reminderTime;
   final void Function() onTap;
 
   @override
@@ -35,9 +38,21 @@ class TaskItemWidget extends StatelessWidget {
           borderRadius: BorderRadius.circular(20.0),
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 24.0),
-            child: Text(
-              text,
-              style: MainStyles.kBlackColor1W500(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  text,
+                  style: MainStyles.kBlackColor1W500(20.0),
+                ),
+                reminderTime != '' ? Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    Strings.reminder + reminderTime,
+                    style: MainStyles.kBlackColor1W500(16.0),
+                  ),
+                ) : const SizedBox(),
+              ],
             ),
           ),
         ),
